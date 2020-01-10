e29cbc92d4f8a98c42d1aec764a9ff156a5e2777
benxincaomu <benxincaomu@foxmail.com>
2020-01-10 14:52:50 +0800

    init

---
+++ src/main/java/io/github/benxincaomu/gitserver/GitServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.benxincaomu.gitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/benxincaomu/gitserver/GitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.benxincaomu.gitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
